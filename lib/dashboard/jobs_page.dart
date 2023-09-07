3f0723a07aec9df33174a9c22c1e255061a68fae
bleymambwe <bleymambwe@gmail.com>
2023-09-07 11:47:24 +0200

    added card and spending widgets

--- lib/dashboard/jobs_page.dart
+++ lib/dashboard/jobs_page.dart
@@ -10,7 +10,144 @@ class JobsScreen extends StatelessWidget {
           SizedBox(
             height: 30,
           ),
-          CustomCard()
+          CustomCard(),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            color: Colors.black,
+            width: double.infinity,
+            height: 1,
+          ),
+          TotalAmountWidget(),
+          Container(
+            color: Colors.black,
+            width: double.infinity,
+            height: 1,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          RecentTransactionsCard()
+        ],
+      ),
+    );
+  }
+}
+
+class RecentTransaction {
+  final IconData icon;
+  final String name;
+  final String date;
+  final double amount;
+
+  RecentTransaction({
+    required this.icon,
+    required this.name,
+    required this.date,
+    required this.amount,
+  });
+}
+
+class RecentTransactionsCard extends StatelessWidget {
+  RecentTransactionsCard({Key? key});
+
+  final List<RecentTransaction> transactions = [
+    RecentTransaction(
+      icon: Icons.factory_outlined,
+      name: 'Pempe Milling',
+      date: '2023-09-13',
+      amount: 2500.00,
+    ),
+    RecentTransaction(
+      icon: Icons.local_drink_sharp,
+      name: 'Pepsi',
+      date: '2023-09-15',
+      amount: 1450.99,
+    ),
+    RecentTransaction(
+      icon: Icons.restaurant,
+      name: 'Hungry Lion',
+      date: '2023-09-14',
+      amount: 1050.00,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: const Color(0x30FFDB58),
+        child: ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (ctx, index) {
+            final transaction = transactions[index];
+            return Column(
+              children: [
+                ListTile(
+                  leading: Icon(transaction.icon),
+                  title: Text(transaction.name),
+                  subtitle: Text(transaction.date),
+                  trailing: Text(
+                    '\K${transaction.amount.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      color: transaction.amount < 0 ? Colors.red : Colors.green,
+                    ),
+                  ),
+                ),
+                Divider(
+                  thickness: 1.0,
+                  color: Colors.grey,
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class TotalAmountWidget extends StatelessWidget {
+  const TotalAmountWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      //color: Colors.amber.withOpacity(0.5),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Column(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total Balance',
+                    style: TextStyle(
+                        //color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.normal),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'K5700',
+
+                    //textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 40,
+                      //color: Colors.brown,
+                      // fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ],
       ),
     );
@@ -32,7 +169,7 @@ class CustomCard extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisSize: MainAxisSize.max,
           children: [
@@ -79,8 +216,8 @@ class CustomCard extends StatelessWidget {
                 ),
                 Image.asset(
                   'assets/account_assets/visa.png',
-                  height: 50,
-                  width: 80,
+                  height: 60,
+                  width: 60,
                 )
               ],
             ),
